dd395d93eff381b464b4c70e567aad44321e2749
jiseop121 <104052493+jiseop121@users.noreply.github.com>
2023-11-15 08:51:27 +0900

    feat: (MenuList) 메뉴리스트 데이터 생성

---
+++ src/main/java/christmas/domain/MenuList.java
@@ -0,0 +1,115 @@
+package christmas.domain;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+
+public enum MenuList {
+    APPETIZER("애피타이저", Map.of(
+            "양송이수프", 6000,
+            "타파스", 5500,
+            "시저샐러드", 8000)),
+    MAIN_COURSE("메인", Map.of(
+            "티본스테이크", 55000,
+            "바비큐립", 54000,
+            "해산물파스타", 35000,
+            "크리스마스파스타", 25000
+    )),
+    DESSERT("디저트", Map.of(
+            "초코케이크", 15000,
+            "아이스크림", 5000
+    )),
+    BEVERAGE("음료", Map.of(
+            "제로콜라", 3000,
+            "레드와인", 60000,
+            "샴페인", 25000
+    ));
+
+    private static final String GIFT_MENU = "샴페인";
+
+    private final String menuCategory;
+    private final Map<String, Integer> menuDetails;
+
+    MenuList(String menuCategory, Map<String, Integer> menuDetails) {
+        this.menuCategory = menuCategory;
+        this.menuDetails = menuDetails;
+    }
+
+    public Map<String, Integer> getMenuDetails() {
+        return menuDetails;
+    }
+
+    public String getMenuCategory() {
+        return menuCategory;
+    }
+
+    public static List<String> getAllMenu() {
+        List<String> allMenu = new ArrayList<>();
+        allMenu.addAll(oneCategoryAllMenu(APPETIZER));
+        allMenu.addAll(oneCategoryAllMenu(MAIN_COURSE));
+        allMenu.addAll(oneCategoryAllMenu(DESSERT));
+        allMenu.addAll(oneCategoryAllMenu(BEVERAGE));
+
+        return allMenu;
+    }
+
+    private static List<String> oneCategoryAllMenu(MenuList menuList) {
+        return new ArrayList<>(menuList.menuDetails.keySet());
+    }
+
+    public static Map<String, Integer> getCategoryCount(Collection<String> menuNames) {
+        Map<String, Integer> menuCategoryCount = initiateMenuCategoryCount();
+
+        for (MenuList menuList : values()) {
+            int categoryCount = oneCategoryCount(menuNames, menuList);
+            menuCategoryCount.put(menuList.menuCategory, categoryCount);
+        }
+        return menuCategoryCount;
+    }
+
+    private static Map<String, Integer> initiateMenuCategoryCount() {
+        return new HashMap<>(Map.of(
+                APPETIZER.menuCategory, 0,
+                MAIN_COURSE.menuCategory, 0,
+                DESSERT.menuCategory, 0,
+                BEVERAGE.menuCategory, 0
+        ));
+    }
+
+    private static int oneCategoryCount(Collection<String> menuNames, MenuList menuList) {
+        int categoryCount = 0;
+        for (String menu : menuNames) {
+            if (menuList.menuDetails.containsKey(menu)) {
+                categoryCount++;
+            }
+        }
+        return categoryCount;
+    }
+
+    public static String getGiftMenuName() {
+        for (String menu : BEVERAGE.menuDetails.keySet()) {
+            if (Objects.equals(menu, GIFT_MENU)) {
+                return menu;
+            }
+        }
+        return "";
+    }
+
+    public static int getGiftMenuPrice() {
+        return BEVERAGE.menuDetails.get(GIFT_MENU);
+    }
+
+    public static int getMenuPrice(String userMenuName) {
+        for (MenuList menuList : values()) {
+            for (String menuName : menuList.menuDetails.keySet()) {
+                if (menuName.equals(userMenuName)) {
+                    return menuList.menuDetails.get(menuName);
+                }
+            }
+        }
+        return 0;
+    }
+}
