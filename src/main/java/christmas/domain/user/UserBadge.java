4dcc2026e933295e7697bcacf0702076db434f89
jiseop121 <104052493+jiseop121@users.noreply.github.com>
2023-11-15 08:59:42 +0900

    feat: (UserBadge) 유저 배지 종류 객체

---
+++ src/main/java/christmas/domain/user/UserBadge.java
@@ -0,0 +1,13 @@
+package christmas.domain.user;
+
+public class UserBadge {
+    private String badge;
+
+    public UserBadge(String badge) {
+        this.badge = badge;
+    }
+
+    public String getMessageBadge() {
+        return badge;
+    }
+}
