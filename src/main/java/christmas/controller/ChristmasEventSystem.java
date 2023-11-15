ac2e260219b4817998d96dbc756953af516ceeef
jiseop121 <104052493+jiseop121@users.noreply.github.com>
2023-11-15 21:58:45 +0900

    refactor: (ChristmasEventSystem) 기능 메서드 분리

--- src/main/java/christmas/controller/ChristmasEventSystem.java
+++ src/main/java/christmas/controller/ChristmasEventSystem.java
@@ -12,10 +12,10 @@ import christmas.view.InputView;
 import christmas.view.OutputView;
 
 public class ChristmasEventSystem {
-    private static final int MAX_INPUT_COUNT = 10;
-
     private final InputParser inputParser = new InputParser();
     private final InputView inputView = new InputView();
+    private final OutputView outputView = new OutputView();
+    private final OutputMessageGenerator outputMessageGenerator = new OutputMessageGenerator();
 
     private UserDay userDay;
     private UserOrderMenu userOrderMenu;
@@ -29,76 +29,61 @@ public class ChristmasEventSystem {
     }
 
     private void inputUserDemand() {
-        InputView inputView = new InputView();
         inputView.displayIntro();
 
-        userDay = inputDayAgain();
-        userOrderMenu = inputOrderMenuAgain();
+        userDay = InputController.inputDayAgain(inputParser, inputView);
+        userOrderMenu = InputController.inputOrderMenuAgain(inputParser, inputView);
     }
 
     private void christmasEvent() {
         OutputView outputView = new OutputView();
-        //인트로
-        outputView.displayMessageIntro(userDay);
-        OutputMessageGenerator outputMessageGenerator = new OutputMessageGenerator();
-        //주문 내역 출력
-        outputView.displayUserMenuList(outputMessageGenerator.getMenuListMessage(userOrderMenu), userOrderMenu);
-        //+추천
+        int calculateAmountBeforeBenefit = 0;
 
-        //총 주문 금액
-        int calculateAmountBeforeBenefit = MenuAmountCalculator.calculateAmountBeforeBenefit(userOrderMenu);
+        outputView.displayMessageIntro(userDay);
+
+        menuListControl();
+        calculateAmountBeforeBenefit = amountBeforeBenefitControl(calculateAmountBeforeBenefit);
+        giftControl(calculateAmountBeforeBenefit);
+        benefitControl();
+        amountAfterBenefitControl();
+        badgeControl();
+    }
+
+    private void menuListControl() {
+        outputView.displayUserMenuList(outputMessageGenerator.getMenuListMessage(userOrderMenu), userOrderMenu);
+    }
+
+    private int amountBeforeBenefitControl(int calculateAmountBeforeBenefit) {
+        calculateAmountBeforeBenefit = MenuAmountCalculator.calculateAmountBeforeBenefit(userOrderMenu);
         userAmount = new UserAmount(calculateAmountBeforeBenefit);
-        //+총 주문 금액 출력
+
         outputView.displayAmountBeforeBenefitMessage(outputMessageGenerator.getAmountBeforeBenefitMessage(userAmount),
                 calculateAmountBeforeBenefit);
 
-        //증정 메뉴
+        return calculateAmountBeforeBenefit;
+    }
+
+    private void giftControl(int calculateAmountBeforeBenefit) {
         userGift = new UserGift(calculateAmountBeforeBenefit, userOrderMenu.getMenuCategoryCount());
-        //+증정 메뉴 출력
         outputView.displayGiftMenuMessage(outputMessageGenerator.getGiftMenuMessage(userGift));
 
-        //혜택 내역
+    }
+
+    private void benefitControl() {
         userBenefit = new UserBenefit(userDay, userOrderMenu, userGift);
         userAmount.applyTotalBenefitAmount(userBenefit.getAllBenefitDiscount());
-        //+혜택 내역 출력
-        outputView.displayBenefitMessage(outputMessageGenerator.getBenefitMessage(userBenefit));
 
-        //총혜택 금액
-        //+총혜택 내역 출력
+        outputView.displayBenefitMessage(outputMessageGenerator.getBenefitMessage(userBenefit));
         outputView.displayTotalBenefitAmountMessage(outputMessageGenerator.getTotalBenefitAmountMessage(userBenefit));
 
-        //예상 결제 금액
-        //+예상 결제 금액 출력
+    }
+
+    private void amountAfterBenefitControl() {
         outputView.displayAmountAfterBenefitMessage(outputMessageGenerator.getAmountAfterBenefitMessage(userAmount));
+    }
 
-        //이벤트 배지
+    private void badgeControl() {
         UserBadge userBadge = new UserBadge(BadgeManager.getBadge(userBenefit.totalBenefitAmount()));
-        //+이벤트 배지 출력
         outputView.displayBadgeMessage(outputMessageGenerator.getBadgeMessage(userBadge), userBenefit, userDay);
-    }
-
-    private UserDay inputDayAgain() {
-        int inputCount = 0;
-        while (inputCount < MAX_INPUT_COUNT) {
-            try {
-                return new UserDay(inputParser.parseInputDay(inputView.inputDay()));
-            } catch (IllegalArgumentException e) {
-                inputCount++;
-            }
-        }
-        throw new RuntimeException();
-    }
-
-    private UserOrderMenu inputOrderMenuAgain() {
-        int inputCount = 0;
-        while (inputCount < MAX_INPUT_COUNT) {
-            try {
-                inputParser.parseInputOrderMenuToList(inputView.inputOrderMenu());
-                return new UserOrderMenu(inputParser.getMenuList(), inputParser.getCountList());
-            } catch (IllegalArgumentException e) {
-                inputCount++;
-            }
-        }
-        throw new RuntimeException();
     }
 }
