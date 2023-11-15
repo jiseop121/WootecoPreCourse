335a3e0336f151d463322f764660670f4fe0ab38
jiseop121 <104052493+jiseop121@users.noreply.github.com>
2023-11-15 18:26:09 +0900

    refactor: (UserGiftTest) 음료 조건 추가

--- src/test/java/christmas/domain/user/UserGiftTest.java
+++ src/test/java/christmas/domain/user/UserGiftTest.java
@@ -3,6 +3,7 @@ package christmas.domain.user;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import christmas.domain.MenuList;
+import java.util.Map;
 import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -14,32 +15,47 @@ class UserGiftTest {
     private static final int MAXIMUM_NOT_GIFT_AMOUNT = 119999;
 
     @ParameterizedTest(name = "[{0}] 총 금액 : {1}")
-    @MethodSource("overAmountTest")
-    @DisplayName("증정 조건 만족")
-    void userGiftTest1(String caseMessage, int amountBeforeBenefit) {
-        UserGift userGift = new UserGift(amountBeforeBenefit);
+    @MethodSource("notOnlyBeverageTest")
+    @DisplayName("음료 외의 메뉴가 있을 때")
+    void userGiftTest1(String caseMessage, int amountBeforeBenefit, int result) {
+        Map<String, Integer> userOrderMenuCategoryCount = Map.of(
+                MenuList.APPETIZER.getMenuCategory(), 1,
+                MenuList.MAIN_COURSE.getMenuCategory(), 1,
+                MenuList.DESSERT.getMenuCategory(), 1,
+                MenuList.BEVERAGE.getMenuCategory(), 1
+        );
+
+        UserGift userGift = new UserGift(amountBeforeBenefit, userOrderMenuCategoryCount);
         assertThat(userGift.getGiftMenuPrice())
-                .isEqualTo(MenuList.GIFT_MENU.getMenuDetails().get("샴페인"));
+                .isEqualTo(result);
     }
 
-    private static Stream<Arguments> overAmountTest() {
+    private static Stream<Arguments> notOnlyBeverageTest() {
         return Stream.of(
-                Arguments.arguments("증정 O", MINIMUM_GIFT_AMOUNT)
+                Arguments.arguments("증정 O 금액", MINIMUM_GIFT_AMOUNT, MenuList.GIFT_MENU.getMenuDetails().get("샴페인")),
+                Arguments.arguments("증정 X 금액", MAXIMUM_NOT_GIFT_AMOUNT, 0)
         );
     }
 
     @ParameterizedTest(name = "[{0}] 총 금액 : {1}")
-    @MethodSource("lowAmountTest")
+    @MethodSource("onlyBeverageTest")
     @DisplayName("증정 조건 불만족")
     void userGiftTest2(String caseMessage, int amountBeforeBenefit) {
-        UserGift userGift = new UserGift(amountBeforeBenefit);
+        Map<String, Integer> userOrderMenuCategoryCount = Map.of(
+                MenuList.APPETIZER.getMenuCategory(), 0,
+                MenuList.MAIN_COURSE.getMenuCategory(), 0,
+                MenuList.DESSERT.getMenuCategory(), 0,
+                MenuList.BEVERAGE.getMenuCategory(), 1
+        );
+
+        UserGift userGift = new UserGift(amountBeforeBenefit, userOrderMenuCategoryCount);
         assertThat(userGift.getGiftMenuPrice())
                 .isEqualTo(0);
     }
 
-    private static Stream<Arguments> lowAmountTest() {
+    private static Stream<Arguments> onlyBeverageTest() {
         return Stream.of(
-                Arguments.arguments("증정 X", MAXIMUM_NOT_GIFT_AMOUNT)
+                Arguments.arguments("증정 X", MINIMUM_GIFT_AMOUNT)
         );
     }
 
