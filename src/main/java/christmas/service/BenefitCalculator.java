772121cac0bd89543f20235357fe5eec015bf1dd
jiseop121 <104052493+jiseop121@users.noreply.github.com>
2023-11-15 09:00:56 +0900

    feat: (BenefitCalculator) 유저 주문의 각 혜택 별 금액 계산 기능

---
+++ src/main/java/christmas/service/BenefitCalculator.java
@@ -0,0 +1,58 @@
+package christmas.service;
+
+import christmas.domain.BenefitCalendar;
+import christmas.domain.user.UserDay;
+import christmas.domain.user.UserGift;
+import christmas.domain.user.UserOrderMenu;
+
+public class BenefitCalculator {
+    private static final int FIRST_DAY = 1;
+    private static final int STAR_DISCOUNT_MONEY = -1000;
+    private static final int WEEKDAY_DISCOUNT_MONEY = -2023;
+    private static final int WEEKEND_DISCOUNT_MONEY = -2023;
+    private static final int NO_BENEFIT = 0;
+
+    public static int applyStarDiscount(UserDay userDay) {
+        if (isCorrectBenefitType(userDay, BenefitCalendar.STAR)) {
+            return STAR_DISCOUNT_MONEY;
+        }
+        return NO_BENEFIT;
+    }
+
+    public static int calculateAmountGiftMenu(UserGift userGift) {
+        return -userGift.getGiftMenuPrice();
+    }
+
+    public static int applyWeekdayDiscount(UserDay userDay, UserOrderMenu userOrderMenu) {
+        if (isCorrectBenefitType(userDay, BenefitCalendar.WEEKDAY)) {
+            return WEEKDAY_DISCOUNT_MONEY * discountCount(userOrderMenu, BenefitCalendar.WEEKDAY.getCategory());
+        }
+        return NO_BENEFIT;
+    }
+
+    public static int applyWeekendDiscount(UserDay userDay, UserOrderMenu userOrderMenu) {
+        if (isCorrectBenefitType(userDay, BenefitCalendar.WEEKEND)) {
+            return WEEKEND_DISCOUNT_MONEY * discountCount(userOrderMenu, BenefitCalendar.WEEKEND.getCategory());
+        }
+        return NO_BENEFIT;
+    }
+
+    public static int applyChristmasDiscount(UserDay userDay) {
+        if (isCorrectBenefitType(userDay, BenefitCalendar.CHRISTMAS_DDAY)) {
+            return -(dayPerThousand(userDay) + 1000);
+        }
+        return NO_BENEFIT;
+    }
+
+    private static int discountCount(UserOrderMenu userOrderMenu, String menuCategory) {
+        return userOrderMenu.getMenuCategoryCount().get(menuCategory);
+    }
+
+    private static int dayPerThousand(UserDay userDay) {
+        return (userDay.getDay() - FIRST_DAY) * 100;
+    }
+
+    private static boolean isCorrectBenefitType(UserDay userDay, BenefitCalendar benefitCalendar) {
+        return userDay.isContainCategory(benefitCalendar.getCategory());
+    }
+}
