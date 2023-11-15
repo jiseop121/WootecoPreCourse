82cc6e05d55cc7085c825187e9a86741a742007d
jiseop121 <104052493+jiseop121@users.noreply.github.com>
2023-11-15 18:27:04 +0900

    refactor: (SystemValidator) 증정품 검증 기능 추가

--- src/main/java/christmas/validator/system/SystemValidator.java
+++ src/main/java/christmas/validator/system/SystemValidator.java
@@ -1,14 +1,19 @@
 package christmas.validator.system;
 
+import christmas.domain.MenuList;
 import christmas.view.message.exception.ErrorMessage;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 public class SystemValidator {
     private static final int MAX_ORDER_COUNT = 1000;
+    private static final int MIN_ORDER_COUNT = 1;
     private static final int FIRST = 1;
     private static final int THIRTY_FIRST = 31;
+    private static final int MINIMUM_AMOUNT_FOR_BADGE = 120000;
+    private static final int ZERO = 0;
 
 //    public void validateOrderMenu(Map<String, Integer> orderMenu) {
 //        if (isOverMaxOrder(orderMenu.values())) {
@@ -22,6 +27,24 @@ public class SystemValidator {
         }
     }
 
+    public static boolean isPossibleGift(int amountBeforeBenefit, Map<String, Integer> userOrderMenuCategoryCount) {
+        return !isOnlyBeverage(userOrderMenuCategoryCount) && !isLessThanMinimumAmount(amountBeforeBenefit);
+    }
+
+    private static boolean isOnlyBeverage(Map<String, Integer> userOrderMenuCategoryCount) {
+        return isZeroCount(userOrderMenuCategoryCount, MenuList.APPETIZER.getMenuCategory())
+                && isZeroCount(userOrderMenuCategoryCount, MenuList.MAIN_COURSE.getMenuCategory())
+                && isZeroCount(userOrderMenuCategoryCount, MenuList.DESSERT.getMenuCategory());
+    }
+
+    private static boolean isZeroCount(Map<String, Integer> userOrderMenuCategoryCount, String category) {
+        return userOrderMenuCategoryCount.get(category).equals(ZERO);
+    }
+
+    private static boolean isLessThanMinimumAmount(int amountBeforeBenefit) {
+        return amountBeforeBenefit < MINIMUM_AMOUNT_FOR_BADGE;
+    }
+
     private static boolean isNotDecemberDay(int day) {
         if (THIRTY_FIRST >= day && day >= FIRST) {
             return false;
@@ -29,9 +52,9 @@ public class SystemValidator {
         return true;
     }
 
-    private static boolean isOverMaxOrder(List<Integer> countList) {
+    private static boolean isOutOfRangeOrderCount(List<Integer> countList) {
         for (int count : countList) {
-            if (count > MAX_ORDER_COUNT) {
+            if (count > MAX_ORDER_COUNT || count < MIN_ORDER_COUNT) {
                 return true;
             }
         }
@@ -39,18 +62,23 @@ public class SystemValidator {
     }
 
     public static void validateMenuName(List<String> menuList, List<Integer> countList) {
-        if (hasDuplicateMenuName(menuList) || isOverMaxOrder(countList)) {
+        if (hasDuplicateMenuName(menuList) || isOutOfRangeOrderCount(countList) || noMenuInMenuList(menuList)) {
             displayAndThrowException(ErrorMessage.INCORRECT_ORDER_FORMAT);
         }
+    }
+
+    private static boolean noMenuInMenuList(List<String> menuList) {
+        for (String menu : menuList) {
+            if (!MenuList.getAllMenu().contains(menu)) {
+                return true;
+            }
+        }
+        return false;
     }
 
     private static boolean hasDuplicateMenuName(List<String> menuList) {
         Set<String> menuWithoutDuplicate = new HashSet<>(menuList);
         return menuWithoutDuplicate.size() < menuList.size();
-    }
-
-    public boolean isDuplicateMenuName(List<String> keySet, String menuName) {
-        return keySet.contains(menuName);
     }
 
     private static void displayAndThrowException(ErrorMessage errorType) {
