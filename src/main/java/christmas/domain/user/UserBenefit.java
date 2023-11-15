4241e1f4024bf5f2bc960f3ab7fab02a021ddf05
jiseop121 <104052493+jiseop121@users.noreply.github.com>
2023-11-15 11:26:34 +0900

    refactor: (UserBenefit) 상수들을 Map으로 변환

--- src/main/java/christmas/domain/user/UserBenefit.java
+++ src/main/java/christmas/domain/user/UserBenefit.java
@@ -1,46 +1,50 @@
 package christmas.domain.user;
 
+import christmas.domain.BenefitCalendar;
+import christmas.domain.MenuList;
 import christmas.service.BenefitCalculator;
+import java.util.HashMap;
+import java.util.Map;
 
 public class UserBenefit {
-    private final int christmas;
-    private final int weekday;
-    private final int weekend;
-    private final int star;
-    private final int gift;
+    private final Map<String, Integer> benefitDiscounts = new HashMap<>(
+            Map.of(
+                    BenefitCalendar.CHRISTMAS_DDAY.getCategory(), 0,
+                    BenefitCalendar.WEEKDAY.getCategory(), 0,
+                    BenefitCalendar.WEEKEND.getCategory(), 0,
+                    BenefitCalendar.STAR.getCategory(), 0,
+                    MenuList.GIFT_MENU.getMenuCategory(), 0
+            )
+    );
 
     public UserBenefit(UserDay userDay, UserOrderMenu userOrderMenu, UserGift userGift) {
-        this.christmas = BenefitCalculator.applyChristmasDiscount(userDay);
-        this.weekday = BenefitCalculator.applyWeekdayDiscount(userDay, userOrderMenu);
-        this.weekend = BenefitCalculator.applyWeekendDiscount(userDay, userOrderMenu);
-        this.star = BenefitCalculator.applyStarDiscount(userDay);
-        this.gift = BenefitCalculator.calculateAmountGiftMenu(userGift);
+        putDiscount(BenefitCalendar.CHRISTMAS_DDAY.getCategory(), BenefitCalculator.applyChristmasDiscount(userDay));
+        putDiscount(BenefitCalendar.WEEKDAY.getCategory(),
+                BenefitCalculator.applyWeekdayDiscount(userDay, userOrderMenu));
+        putDiscount(BenefitCalendar.WEEKEND.getCategory(),
+                BenefitCalculator.applyWeekendDiscount(userDay, userOrderMenu));
+        putDiscount(BenefitCalendar.STAR.getCategory(), BenefitCalculator.applyStarDiscount(userDay));
+        putDiscount(MenuList.GIFT_MENU.getMenuCategory(), BenefitCalculator.calculateAmountGiftMenu(userGift));
     }
 
-    public int totalBenefitAmount() {
-        return christmas + weekend + weekday + star + gift;
+    public void putDiscount(String benefitType, int discount) {
+        benefitDiscounts.put(benefitType, discount);
+    }
+
+    public int getAllBenefitDiscount() {
+        return sumAllBenefitDiscount();
+    }
+
+    private int sumAllBenefitDiscount() {
+        int allBenefit = 0;
+        for (String benefitType : benefitDiscounts.keySet()) {
+            allBenefit += benefitDiscounts.get(benefitType);
+        }
+
+        return allBenefit;
     }
 //
 //    public int getMessageAllBenefit() {
 //    }
 
-    public int getChristmas() {
-        return christmas;
-    }
-
-    public int getGift() {
-        return gift;
-    }
-
-    public int getStar() {
-        return star;
-    }
-
-    public int getWeekday() {
-        return weekday;
-    }
-
-    public int getWeekend() {
-        return weekend;
-    }
 }
