09e38dc767adb15712bdc7482d62fd9bac9182c2
jiseop121 <104052493+jiseop121@users.noreply.github.com>
2023-11-15 18:27:27 +0900

    refactor: (AmountCalculator)필요없는 기능 제거

--- src/main/java/christmas/service/AmountCalculator.java
+++ src/main/java/christmas/service/AmountCalculator.java
@@ -1,14 +1,9 @@
 package christmas.service;
 
 import christmas.domain.MenuList;
-import christmas.domain.user.UserBenefit;
 import christmas.domain.user.UserOrderMenu;
 
 public class AmountCalculator {
-    public static int calculateTotalBenefitAmount(UserBenefit userBenefit) {
-        return 0;
-    }
-
     public static int calculateAmountBeforeBenefit(UserOrderMenu userOrderMenu) {
         int totalPrice = 0;
         for (String menuName : userOrderMenu.getOrderMenuNames()) {
