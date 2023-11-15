b1cdbc9714ecdd7d020918e10ad7f1bdbc2d5e91
jiseop121 <104052493+jiseop121@users.noreply.github.com>
2023-11-15 18:27:54 +0900

    refactor: (UserGift) 검증 기능 분리

--- src/main/java/christmas/domain/user/UserGift.java
+++ src/main/java/christmas/domain/user/UserGift.java
@@ -1,49 +1,42 @@
 package christmas.domain.user;
 
 import christmas.domain.MenuList;
-import christmas.view.message.view.OutputMessage;
+import christmas.validator.system.SystemValidator;
+import java.util.Map;
 
 public class UserGift {
-    private static final int MINIMUM_AMOUNT_FOR_BADGE = 120000;
-    private final String giftMenuName;
-    private final int giftMenuPrice;
+    private String giftMenuName = "";
+    private int giftMenuPrice = 0;
     private static final int GIFT_COUNT = 1;
 
-    public UserGift(int amountBeforeBenefit) {
-        this.giftMenuName = giftMenu(amountBeforeBenefit);
-        this.giftMenuPrice = giftMenuPrice(amountBeforeBenefit);
+    public UserGift(int amountBeforeBenefit, Map<String, Integer> userOrderMenuCategoryCount) {
+        setUserGift(amountBeforeBenefit, userOrderMenuCategoryCount);
+    }
+
+    private void setUserGift(int amountBeforeBenefit, Map<String, Integer> userOrderMenuCategoryCount) {
+        if (SystemValidator.isPossibleGift(amountBeforeBenefit, userOrderMenuCategoryCount)) {
+            this.giftMenuName = giftMenu();
+            this.giftMenuPrice = giftMenuPrice();
+        }
     }
 
     public int getGiftMenuPrice() {
         return giftMenuPrice;
     }
 
-    public String getGiftMenuAndCount() {
-        StringBuilder sb = new StringBuilder();
-        sb.append(giftMenuName)
-                .append(OutputMessage.SPACE_BAR.getMessage())
-                .append(GIFT_COUNT)
-                .append(OutputMessage.COUNT_UNIT.getMessage());
-
-        return sb.toString();
+    public String getGiftMenuNameMessage() {
+        return giftMenuName;
     }
 
-    private String giftMenu(int amountBeforeBenefit) {
-        if (isAvailableGetGift(amountBeforeBenefit)) {
-            return MenuList.getGiftMenuName();
-        }
-        return "";
+    public String getGiftCountMessage() {
+        return String.valueOf(GIFT_COUNT);
     }
 
-    private int giftMenuPrice(int amountBeforeBenefit) {
-        if (isAvailableGetGift(amountBeforeBenefit)) {
-            return MenuList.getGiftMenuPrice();
-        }
-        return 0;
+    private String giftMenu() {
+        return MenuList.getGiftMenuName();
     }
 
-    private boolean isAvailableGetGift(int amountBeforeBenefit) {
-        return amountBeforeBenefit >= MINIMUM_AMOUNT_FOR_BADGE;
+    private int giftMenuPrice() {
+        return MenuList.getGiftMenuPrice();
     }
-
 }
